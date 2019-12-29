1cf51b3ece92d63b8594c8d2f252c7fa0d89adab
Alain Kaim <48492533+AlainKaim@users.noreply.github.com>
2019-12-29 15:35:03 +0100

    First version

--- README.md
+++ README.md
@@ -1,2 +1,97 @@
 # homebridge-heatzy-as-switch
- Homebridge plugin for Heatzy devices, considered as switches
+ Homebridge plugin for Heatzy devices, considered as switches.
+ 
+Heatzy uses the 'fil pilote' protocol to control an electric heater, with 4 states : 
+
+* Confort  : temperature set on the heater
+* Eco : temperature 3°C to 4°C below Confort
+* Hors-gel : temperature set to ~7°C
+* Off.
+
+In this plugin, every Heatzy device is a switch, with these values : 
+
+* `On` : Confort
+* `Off` : Eco (or any any other state different from Confort, if set outside of Homekit).
+
+If you set the device from the Home app, `on` will set the heater to Confort, and `off` to Eco.
+If you set it from the Heatzy app, or from the hardware button, Confort will be displayed in the Home app as `on`. Any other state (Eco, Hors-Gel or Off) will be displayed as `off`.
+## How to use it ?
+Obviously, it is nice to have all your devices under the Home app.
+I have chosen to put my Heatzy devices in the same (virtual) room, called "Heating" : it is easier to check what is going on.
+As for any Home devices, you can group Heatzy devices, and control them as one. And you can change how the switch looks in the Home app (Fan is my favourite). 
+
+But what is really nice is to control Heatzy from Siri : "Siri, allume le convecteur chambre" !
+
+And to include these devices in automations : for instance, I turn my heaters to Eco when we are all out of our home (Use Eve app for more sophisticated automations).
+
+Heatzy app is still very useful for programation, and for specific modes (Holydays and boost). And, of course, to set to Hors-gel and Off state.
+## Installation
+Install this plugin using `npm i -g homebridge-heatzy-as-switch`.
+
+Update the `config.json` file of your Homebridge setup, by modifying the sample configuration below.
+
+## Updating
+Update to the latest release of this plugin using `npm i -g homebridge-tuya-lan`.
+
+## Configurations
+The configuration parameters to enable your devices would need to be added to `accessories` section of the Homebridge configuration file. One block is necessary for each Heatzy device.
+
+```json5
+{
+    ...
+            "accessories": [
+                {
+                    "accessory": "HeatzyAsSwitch",
+                    "name": "Bedroom heater",
+                    "username": "XXX",
+                    "password": "XXX",
+                    "did": "011233455677899abbcd",
+                    "interval": 60,
+                    "trace" : false
+                }
+            ]
+    ...
+}
+```
+#### Parameters
+* `accessory ` is required, with `HeatzyAsSwitch` value.  
+* `name` (required) is anything you'd like to use to identify this device. You can always change the name from within the Home app.
+* `username` and `password` (required) are the credentials you use in the Heatzy app.
+* `did` (required) is the parameter for your device. See below how to get it.
+* `interval` (optional) is how often (in seconds) the plugin will ask Heatzy servers the state of your device, which is necessary when you change the state from outside of Homekit. Default is 60s.
+* `trace` (optional) displays the main events in homebridge log . Default is false.
+
+
+## How to find the  `did` of your devices
+In your terminal, enter the two commands below.
+
+For the first one, you will have to replace USERNAME and PASSWORD by your credentials used in the Heatzy app.
+In return, you should get a `token` : you will use it in the second command, to replace YOURTOKEN.
+
+The second command will return many datas. For each Heatzy device, you must find this piece of information : `"did": "011233455677899abbcd"`. To know wich `did` is for which device, you will find another piece of informatation close to it:` "dev_alias": "Name"`. The Name is the one used in the Heatzy app.
+(You can choose a different name in homebridge configuration file, if you wish).
+
+
+`curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' --header 'X-Gizwits-Application-Id: c70a66ff039d41b4a220e198b0fcc8b3' -d '{ "username": "USERNAME", "password": "PASSWORD", "lang": "en" }' 'https://euapi.gizwits.com/app/login'`
+
+`curl -X GET --header 'Accept: application/json' --header 'X-Gizwits-User-token: YOURTOKEN' --header 'X-Gizwits-Application-Id: c70a66ff039d41b4a220e198b0fcc8b3' 'https://euapi.gizwits.com/app/bindings?limit=20&skip=0'`
+## Limits
+### Works only with Heatzy Pilote
+Other Heatzy devices (Flam, Inea, ...) are not supported. Tested with Heatzy Pilote V2 devices only, but should work with V1.
+### No access to Off and Hors-Gel from Home app
+I have not found a simple and meaningful way to set these states from the Home app. Yet...
+Anyway, they are not used very often, so the Heatzy app may be enough to control them.
+### Accessory, no platform
+Platform implementation would be a better solution (not need to to get `did`, ...), but I would have to learn more about coding a homebridge plugin. One day, may be....
+### Sometimes, Home app displays the wrong state
+It happens mostly when you do many updates in a short period of time. The device is correctly set : you can check it on the device, and in the Heatzy app. But, for reasons I do not know, the Heatzy servers may take some time (minutes, or hours...) to reflect the new state. Until this is done, Home app wil display the old state. I hope Heatzy will fix this. In the meantime, usually, a new order sent the device will set things right.
+### Errors
+Sometime, Heatzy servers are not working properly for a short period time. During these periods, the plugin cannot control or even display the state of the devices. Errors will be displayed in homebridge log.
+From my experience, these periods are usually very short, so this is not a real problem.
+
+## Credit
+To create this plugin, I learnt a lot, from many sources  : coding in NodeJS, using github and npm, understanding homebridge and axios, ...
+
+A lot of thanks to nfarina for homebridge !!!
+
+And to all the plugin devs who are sharing their code.
---
+++ index.js
@@ -0,0 +1,207 @@
+'use strict'
+
+// We will use axios and url modules to perform our HTTP requests.
+const url = require('url');
+const axios = require('axios');
+
+// From Heatzy API : https://drive.google.com/drive/folders/0B9nVzuTl4YMOaXAzRnRhdXVma1k
+// https://heatzy.com/blog/tout-sur-heatzy
+const heatzyUrl = "https://euapi.gizwits.com/app/";
+const loginUrl = url.parse(heatzyUrl + "login")
+const heatzy_Application_Id = "c70a66ff039d41b4a220e198b0fcc8b3"
+
+let Service, Characteristic
+
+module.exports = (homebridge) => {
+  /* this is the starting point for the plugin where we register the accessory */
+  Service = homebridge.hap.Service
+  Characteristic = homebridge.hap.Characteristic
+  homebridge.registerAccessory('homebridge-heatzy-as-switch', 'HeatzyAsSwitch', SwitchAccessory)
+}
+
+function SwitchAccessory(log, config) {
+    /* assign both log and config to properties on 'this' class so we can use them in other methods */
+    this.log = log
+    this.config = config
+// Get informations from config file
+    this.getUrl = url.parse(heatzyUrl + "devdata/" + config['did'] + "/latest");
+  	this.postUrl = url.parse(heatzyUrl + "control/" + config['did']) ;
+  	this.name = config["name"];
+  	this.username = config["username"];
+  	this.password = config["password"];
+  	this.interval = config["interval"] || 60;  	// default value is 60s
+  	this.trace = config["trace"] || false;	//default value is false (no trace)
+// Heatzy Token management
+  	this.heatzyToken = "";
+  	this.heatzyTokenExpire_at = Date.now() - 10000; // In ms since epoch time (January 1, 1970). Initial value is 10s in the past, to force login and refresh of token
+
+  	this.state = null; // Last state of the device, as known on Heatzy servers
+  	this.updatedAt = 0; // Last time the state was updated on Heatzy server (not used)
+
+     /* Create a new information service. This just tells HomeKit about our accessory. */
+    this.informationService = new Service.AccessoryInformation()
+        .setCharacteristic(Characteristic.Manufacturer, 'Heatzy')
+        .setCharacteristic(Characteristic.Model, 'Heatzy Pilote V2')
+        .setCharacteristic(Characteristic.SerialNumber, ' unknown')
+  // Create the switch service
+	  this.service = new Service.Switch(this.config.name);
+  // Add to the switch service the functions called to modify it characteristic
+	  this.service
+		.getCharacteristic(Characteristic.On)
+		.on('get', this.getOnCharacteristicHandler.bind(this))
+		.on('set', this.setOnCharacteristicHandler.bind(this));
+
+	this.updateState()  // Get the current state of the device, and update HomeKit
+	setInterval(this.updateState.bind(this), 1000*this.interval)  // The state of the device will be checked every this.interval seconds
+	this.log("starting HeatzyAsSwitch...");
+}	//SwitchAccessory
+
+/////////// Supporting functions, for updating the token, getting and setting the state of a device
+async function updateToken (device) {  // This function get the Heatzy token, and store it
+	const me = device;
+	try {
+  		const response = await axios ({
+ 			method: 'post',
+  			url: loginUrl,
+   			headers: {
+  				'X-Gizwits-Application-Id': heatzy_Application_Id
+ 			},
+   			data: {
+			  "username" : me.username,
+			  "password" : me.password,
+			  "lang": "en"
+			}
+  		})
+//	me.log(response);
+    if (response.status == 200) {
+    	me.heatzyToken = response.data.token;
+    	me.heatzyTokenExpire_at = 1000 * response.data.expire_at; //The API returns a date in seconds, but javascript works in ms...
+    }
+    else { // Useless ? all status != 2xx will be errors
+    	me.log ('Error - returned code not 200: ' + response.status + ' ' + response.statusText + ' ' + response.data.error_message);
+    }
+  }
+   catch (error) {
+    // handle error
+//    me.log(error);
+     me.log ('Error : ' + error.response.status + ' ' + error.response.statusText );
+     me.log ('Error - Plugin unable to login to Heatzy server, and will not work');
+  }
+} // updateToken
+
+
+async function  getState(device) { //return the state of the device as a boolean. Or null if undefined
+	const me = device;
+   	var state = false;
+	try {
+  		const response = 	await axios.get (me.getUrl, {
+  			headers: {'X-Gizwits-Application-Id': heatzy_Application_Id}
+  		})
+    // handle success
+  //	console.log(response);
+    if (response.status == 200) {
+		if (response.data.attr.mode == "cft") {state = true	}
+    }
+    else { // Useless ? all status != 2xx will be errors
+    	me.log ('Error - returned code not 200: ' + response.status + ' ' + response.statusText + ' ' + response.data.error_message);
+    	state = null
+    }
+  } catch (error) {
+    // handle error
+//    console.log(error.response);
+     me.log ('Error : ' + error.response.status + ' ' + error.response.statusText );
+     state = null
+  } finally {return state}
+} // getState
+
+
+async function  setState(device, state) { //Set the state of the device, and return it if successful. Or null if failed
+	const me = device;
+	if (me.heatzyTokenExpire_at < Date.now()) {await updateToken (device)}; // Forced at first run, and then calld only if token is expired
+	let mode = "eco";
+	if (state) {mode = "cft"};
+	try {
+  		const response = await axios ({
+ 			method: 'post',
+  			url: me.postUrl,
+   			headers: {
+  				'X-Gizwits-Application-Id': heatzy_Application_Id,
+				'X-Gizwits-User-token': me.heatzyToken
+ 			},
+   			data: {
+			  "attrs" : {
+			  "mode": mode
+			  }
+			}
+  		})
+//	me.log(response);
+    if (response.status == 200) {
+    }
+    else { // Useless ? all status != 2xx will be errors
+    	me.log ('Error - returned code not 200: ' + response.status + ' ' + response.statusText + ' ' + response.data.error_message);
+    	state = null
+    }
+  } catch (error) {
+    // handle error
+//    me.log(error);
+     me.log ('Error : ' + error.response.status + ' ' + error.response.statusText );
+     state = null
+  } finally {return state}
+} // setState
+
+/////// Implementation of the homebridge services
+
+SwitchAccessory.prototype.updateState = async function() {
+  	var state = await getState(this);
+  //	if (this.trace) { this.log('DEBUG - Mode was ' + this.state + '. Updating it to : ' + state)} // Uncomment for easier debugging...
+  	if (state !== null ) {
+  		if (this.state === null) {this.state = state}  //Initialize for first run
+  		if (state !== this.state) {	// If device state has changed since last update
+			if (this.trace) {
+				this.log('State has changed from: ' + this.state + ' to ' + state);
+			};
+			this.state = state;   // Update last state
+			this.service.updateCharacteristic(Characteristic.On, state);  // update HomeKit
+		}
+   	}
+	// If state  is null (unavailable) , do nothing because the device state will be updated at the next call
+  } // SwitchAccessory.prototype.updateState
+
+
+SwitchAccessory.prototype.getOnCharacteristicHandler = async function(callback) {
+  	var state = await getState(this);
+  	if (this.trace) {
+		this.log('HomeKit asked for state (true for cft, false for eco): ' + state)
+	}
+  	if (state != null) {
+  		callback (null, state);
+   	}
+   	else {
+		this.log("Error : Unavailable state");
+		callback (true);
+   	}
+} // SwitchAccessory.prototype.getOnCharacteristicHandler
+
+
+
+SwitchAccessory.prototype.setOnCharacteristicHandler = async function(value, callback) {
+   	var state = await setState(this, value);
+  	if (this.trace) {
+		this.log('HomeKit changed state to (true for cft, false for eco): ' + state)
+	}
+//  This code works only when the new state is correctly reflected on Heatzy servers.
+// It is not always the case.
+  	if (state != null) {
+  		callback (null, state);
+   	}
+   	else {
+		this.log("Error - Cannot change state");
+		callback (true);
+   	}
+}  // SwitchAccessory.prototype.setOnCharacteristicHandler
+
+
+SwitchAccessory.prototype.getServices = function() {
+this.log ("Init Services...")
+  return [this.service, this.informationService];
+} // SwitchAccessory.prototype.getServices
